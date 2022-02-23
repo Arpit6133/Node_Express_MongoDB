a4a3a838ed43192b56c830aaa3c602482ce77bca
Arpit6133 <arpitdayma5@gmail.com>
2022-02-23 21:16:22 +0530

    Node Callback Hell and Promises

--- operations.js
+++ operations.js
@@ -1,40 +1,23 @@
 const assert = require('assert');
 const Collection = require('mongodb/lib/collection');
 
-
-exports.insertDocument = (db , document, collection, callback) => {
+exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document,(err,result) => {
-        assert.equal(err,null);
-        console.log("Inserted" + result.result.n +
-            " documents into the collection" + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
-exports.findDocument = (db , document, collection, callback) => {
-    const col = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err,null);
-        callback(docs);
-    });
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db , document, collection, callback) => {
-    const col = db.collection(collection);
-    coll.deleteOne(document, (err,result) => {
-        assert.equal(err, null);
-        console.log("Removed the document " , document);
-        callback(result);
-    });
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db , document, collection, callback) => {
-    const col = db.collection(collection);
-    coll.updateOne(document,{ $set: update}, null ,(err,result) => {
-        assert.equal(err,null);
-        console.log("Updated the document with ",update);
-        callback(result);
-    });
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
 };
 
